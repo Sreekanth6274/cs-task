1bedd874d3cf8e10d8ed868215de7fbb0c166dad
Sreekanth6274 <164642286+Sreekanth6274@users.noreply.github.com>
2024-06-08 09:34:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import'./App.css';
+import Fly from "./Components/Fly"
+import Travelling from './Components/Travelling';
+import Button from './Components/Button'
+
+function App() {
+  return (
+    <div>
+     <Fly/>
+     <Travelling/>
+     <Button/>
+     
+    </div>
+  )
+}
+
+export default App;
